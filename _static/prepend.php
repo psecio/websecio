5433eebc46852ce59f6d0fa3389f58a1d9260859
Chris Cornutt <enygma@phpdeveloper.org>
2015-12-14 17:46:06 -0600

    updating prepend with more headers

--- _static/prepend.php
+++ _static/prepend.php
@@ -1,3 +1,6 @@
 <?php
 // Some custom headers - included via prepend_file
 header('Strict-Transport-Security: strict-transport-security: max-age=31536000; includeSubdomains');
+header('X-Frame-Options: sameorigin');
+header('X-XSS-Protection: 1;mode=block');
+header('X-Content-Type-Options: nosniff');
